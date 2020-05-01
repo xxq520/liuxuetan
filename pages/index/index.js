6386a5fd728f675daec8c55e896e32e2cacc6e38
xxq520 <2093001584@qq.com>
2020-05-01 18:57:20 +0800

    合并代码提交

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,9 +4,20 @@ const app = getApp()
 
 Page({
   data: {
-
+      // 是否显示关注选框
+      isShow:false
   },
-
+  guanzhu(type,type1){
+    if(type1){
+      this.setData({
+        isShow:true
+      })
+    }else{
+      this.setData({
+        isShow:true
+      })
+    }
+  },
   onLoad: function () {
 
   },
